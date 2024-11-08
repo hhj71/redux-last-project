ef3e60174916476aa5af156d04d09c37582038b3
hhj71 <chhj017@naver.com>
2024-11-08 17:32:21 +0900

    11/08 redux 공부

---
+++ src/reducers/boardReducer.js
@@ -0,0 +1,60 @@
+import
+  {
+      FETCH_BOARD_LIST,
+      FETCH_BOARD_DETAIL,
+      FETCH_BOARD_DELETE,
+      FETCH_BOARD_UPDATE_OK,
+      FETCH_BOARD_INSERT,
+      FETCH_BOARD_UPDATE} from "../actions/types";
+// [] => List
+// {} => 객체 => VO , Map
+// 프로그램은 => 재사용
+// 객체지향 프로그램 => 재사용
+const boardState={
+    board_list:{},
+    board_detail:{},
+    board_update:{},
+    result:{}
+}
+// dispatch(action) => action = type(상수) , payload(실제 서버데이터)
+// spring-boot : maven(X) , gradle
+// appication.properties , application.yml
+export default function(state=boardState, action){
+    console.log(action.type);
+    console.log(action.payload);// 서버에서 전송된 값
+    switch (action.type) {
+        case FETCH_BOARD_LIST: // 목록 요청 => 사용자 요청 구분
+            console.log(action.payload);
+            return {
+               ...state,
+               board_list:action.payload
+            }
+        case FETCH_BOARD_INSERT:
+            return {
+                ...state,
+                result:action.payload
+            }
+        case FETCH_BOARD_DETAIL:
+            return {
+                ...state,
+                board_detail: action.payload
+            }
+        case FETCH_BOARD_DELETE:
+            return {
+                ...state,
+                result:action.payload
+            }
+        case FETCH_BOARD_UPDATE:
+            return {
+                ...state,
+                board_update:action.payload
+            }
+        case FETCH_BOARD_UPDATE_OK:
+            return {
+                ...state,
+                result:action.payload
+            }
+        default:
+            return state;
+    }
+}
